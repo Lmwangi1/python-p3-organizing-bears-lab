922f27eb5c3602f29758af5c608592a570e28d6e
lucy <lmwangi619@gmail.com>
2025-07-12 16:53:48 +0300

    p3-SQL1

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'brown', 'calm', TRUE),
+('Rowdy', 4, 'M', 'black', 'aggressive', TRUE),
+('Tabitha', 3, 'F', 'blonde', 'friendly', TRUE),
+('Sergeant Brown', 6, 'M', 'brown', 'serious', TRUE),
+('Melissa', 2, 'F', 'white', 'cheerful', TRUE),
+('Grinch', 7, 'M', 'green', 'grumpy', TRUE),
+('Wendy', 4, 'F', 'gray', 'gentle', TRUE),
+(NULL, 1, 'F', 'black', 'quiet', TRUE);
